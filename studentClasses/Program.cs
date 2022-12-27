10469ef1e170802d5e54673fb2de79f70e24bce8
Şermin Atmaca <117165693+serminatmaca@users.noreply.github.com>
2022-12-27 21:36:16 +0300

    class
    class oluşturma ve veri ekleme

---
+++ studentClasses/Program.cs
@@ -0,0 +1,49 @@
+﻿
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+
+        Student student = new Student();
+
+        student.Name = " sermin ";
+        student.SurName = " ATMACA ";
+        student.Year = 29;
+        student.Class = 8;
+        student.Number = 456;
+        student.Epsode = " ky ";
+
+        Console.WriteLine(student.Name + " - " + student.SurName + " - " + student.Year + " - " + student.Class + " -" + student.Number + " - " + student.Epsode + " - ");
+
+        Console.ReadKey();
+
+
+
+
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public int Year { get; set; }
+
+        public int Class { get; set; }
+
+        public int Number { get; set; }
+
+        public string Epsode { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
